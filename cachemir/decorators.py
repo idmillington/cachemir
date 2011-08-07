f359ddaca71e788589bb0121c431616c984f1084
Ian Millington <idmillington@googlemail.com>
2011-08-07 01:38:07 +0100

    Made system work with multiple caches on an object.

--- cachemir/decorators.py
+++ cachemir/decorators.py
@@ -15,6 +15,58 @@ def cache(name, uid_fn=lambda x:str(hash(x)), storage=None, hash_fn=None):
         return fn
     return _decorator
 
+def __init_cache_fn(Class, fn):
+    # Find the cache configuration
+    name, uid_fn, storage_obj, hash_fn = fn.__cache_config
+
+    # Use default storage in a temporary directory if we don't
+    # have one.
+    if storage_obj is None:
+        storage_obj = storage.CacheFileStorage()
+
+    # Create the get function
+    def __get(self, *args, **kws):
+        # Calculate the data hash.
+        if hash_fn is not None:
+            data_hash = hash_fn(self, *args, **kws)
+        else:
+            elements = [uid_fn(self)]
+            elements.extend([
+                    str(hash(arg)) for arg in args
+                    ])
+            elements.extend([
+                    (key, str(hash(kws[key])))
+                    for key in sorted(kws.keys())
+                    ])
+
+            # Do a SHA1 on the Python repr of the elements.
+            data_hash = hashlib.sha1(repr(elements)).hexdigest()
+
+        # See if we have copy.
+        if not storage_obj.has(data_hash):
+            # Do this the explicit way (rather than the newer
+            # 'with' statement) as not every file-like may be a
+            # context.
+            out = storage_obj.out(data_hash)
+            try:
+                # Create the data.
+                fn(self, out, *args, **kws)
+            finally:
+                out.close()
+
+            # Sanity check
+            assert storage_obj.has(data_hash)
+
+        # Created or not, return the file-like for the data.
+        return storage_obj.get(data_hash)
+
+    # Save the get function
+    __get.__name__ = name
+    setattr(Class, name, __get)
+
+    # Remove the config data.
+    del fn.im_func.__cache_config
+
 def cache_init(Class):
     """
     Go through each cached method and add the relevant methods.
@@ -22,56 +74,7 @@ def cache_init(Class):
     for fn_name in dir(Class):
         fn = getattr(Class, fn_name)
         if callable(fn) and hasattr(fn, "__cache_config"):
-            # Find the cache configuration
-            name, uid_fn, storage_obj, hash_fn = fn.__cache_config
-
-            # Use default storage in a temporary directory if we don't
-            # have one.
-            if storage_obj is None:
-                storage_obj = storage.CacheFileStorage()
-
-            # Create the get function
-            def __get(self, *args, **kws):
-                # Calculate the data hash.
-                if hash_fn is not None:
-                    data_hash = hash_fn(self, *args, **kws)
-                else:
-                    elements = [uid_fn(self)]
-                    elements.extend([
-                            str(hash(arg)) for arg in args
-                            ])
-                    elements.extend([
-                            (key, str(hash(kws[key])))
-                            for key in sorted(kws.keys())
-                            ])
-
-                    # Do a SHA1 on the Python repr of the elements.
-                    data_hash = hashlib.sha1(repr(elements)).hexdigest()
-
-                # See if we have copy.
-                if not storage_obj.has(data_hash):
-                    # Do this the explicit way (rather than the newer
-                    # 'with' statement) as not every file-like may be a
-                    # context.
-                    out = storage_obj.out(data_hash)
-                    try:
-                        # Create the data.
-                        fn(self, out, *args, **kws)
-                    finally:
-                        out.close()
-
-                    # Sanity check
-                    assert storage_obj.has(data_hash)
-
-                # Created or not, return the file-like for the data.
-                return storage_obj.get(data_hash)
-
-            # Save the get function
-            __get.__name__ = name
-            setattr(Class, name, __get)
-
-            # Remove the config data.
-            del fn.im_func.__cache_config
+            __init_cache_fn(Class, fn)
 
     # The class is modified, but we don't need to return a proxy.
     return Class
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import subprocess
 import unittest
 import cachemir
 
@@ -16,11 +17,36 @@ class CachemirTestCase(unittest.TestCase):
                 location, with the given parameters.
                 """
                 out.write("This is my output.")
+
+            @cachemir.cache('get_qr')
+            def _create_qr(self, out):
+                """
+                Creates a qr code for  url based. Requires qrencode.
+                """
+                cmd = [
+                    "qrencode",
+                    "-o", "-",
+                    "-m", "0",
+                    "http://github.com/idmillington/cachemir"
+                    ]
+                proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+                out.write(proc.stdout.read())
+                out.close()
+
         self.Foo = Foo
 
     def test_get_output(self):
         f = self.Foo()
         self.assertEqual(f.get_foo_pdf(1,2,3).read(), "This is my output.")
+
+    def test_external(self):
+        """
+        Tests the generation with an external routine (qrencode in this case).
+        """
+        f = self.Foo()
+        qr = f.get_qr().read()
+        self.assert_(len(qr) > 100)
+        self.assertEqual(qr[:4], '\x89PNG')
 
 if __name__ == '__main__':
     unittest.main()
